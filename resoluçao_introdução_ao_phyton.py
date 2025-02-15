a80d5dfb3ad92c86d40da6bcd0397c66a38faf8f
GustavoRodri1213 <102840825+GustavoRodri1213@users.noreply.github.com>
2025-02-15 16:34:07 -0300

    Add files via upload

---
+++ resoluçao_introdução_ao_phyton.py
@@ -0,0 +1,23 @@
+# Questão 01
+'''
+x = "Olá,mundo"
+y = 10
+z = 10.5
+a = (10 == 10)
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+'''
+# Questão 02
+'''
+valor_compras = 25.60 #Valor da unidade
+desconto = valor_compras * 0.2 #Desconto de 20%
+quantidade_itens = 5 
+valor_desconto = (valor_compras - desconto) * quantidade_itens
+
+
+print("O valor das compras com o desconto foi de",valor_desconto,"R$")
+print("O custo médio de cada item foi de",valor_desconto/quantidade_itens,"R$")
+'''
