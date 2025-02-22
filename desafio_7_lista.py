90e42e1fa04cfb8077bc6a0b58fc9ff41dec7749
Gusta_Rodri <102840825+GustavoRodri1213@users.noreply.github.com>
2025-02-21 21:06:00 -0300

    Add files via upload

---
+++ desafio_7_lista.py
@@ -0,0 +1,31 @@
+# DESAFIO. Uma string ( str ) também pode ser percorrida utilizando o for
+
+# for x in "abc":
+# print(x)
+#  Vai imprimir:
+#  a
+#  b
+#  c
+
+# Escreva um programa que solicite uma string para o usuário e imprima quantas
+# vezes cada letra aparece na palavra. Por exemplo:
+
+# "banana"
+#  O resultado deve ser
+# {
+# 'a': 3,
+# 'b': 1,
+# 'n': 2
+# }
+
+palavra = input("Informe uma palavra:\n")
+
+contagem = { }
+
+for letra in palavra:
+    if letra in contagem:
+        contagem[letra] += 1  
+    else:
+        contagem[letra] = 1   
+print(contagem)
+
